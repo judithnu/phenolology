ecda2357598f2423e2753243fc2f221da782f4df
Susannah <susannah.tysor@gmail.com>
2017-06-14 10:12:20 -0700

    Made simple toy phenology and climate dataset

---
+++ toydata.R
@@ -0,0 +1,12 @@
+# Make simple dataset for building & testing models
+
+## Data for 1 tree at one site with constant temperatures and yes/no phenology data
+n = 5 # How many days
+
+dat <- data.frame(
+    day = seq(1:n),
+    temp = rep(5, n),
+    tree = 1,
+    pct_budburst = c(0,0,0,1,1),
+    accumulated_temp = NA)
+
