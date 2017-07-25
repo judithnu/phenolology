77bdb9ca93a586d8b9bb1105b28ecae88ee61606
Susannah <susannah.tysor@gmail.com>
2017-07-25 13:44:44 -0700

    Worked on building toy tree level cone phenology generator
    To help think thru how to connect my data to process model

--- simulateddata.R
+++ simulateddata.R
@@ -24,3 +24,26 @@ summary(logit)
 plot(fauxdat$atemp, fauxdat$fl)
 curve(predict(logit, data.frame(atemp = x), type = "resp"), add = TRUE)
 
+##
+
+## tree level toy
+### Tree has 10 cones. Over a 10 day period, cones become active for some amount of time, then finish. Cones become active at DD = 20 with some error.
+
+temp = 5
+temp_crit = 20
+
+tdat <- data.frame(day = c(1:10), temp = 5)
+tdat$atemp <- cumsum(tdat$temp)
+
+temp_crit_ind <- rnorm(n = 10, mean = temp_crit, sd = temp_crit/4) #critical temperatures for individual cones
+
+cdat <- data.frame(cone = c(1:10), temp_crit_ind = temp_crit_ind)
+
+#######drafty not yet working
+day_crit <- sapply(cdat$temp_crit_ind, get_closest_bigger_number, vector = tdat$atemp)
+
+get_closest_bigger_number <- function(value, vector) {
+    diffs <- abs(value - vector)
+    bigs <- which(vector > value)
+    which.min(diffs[bigs])
+}
