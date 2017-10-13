c92db26256e29d3b0d492f21ce85a7067beda7cd
C. Susannah Tysor <susannah@pobox.com>
2017-10-12 17:16:31 -0700

    Made transition probabilities a function of heat sum.
    In a highly artificial way.

--- processsimulation.R
+++ processsimulation.R
@@ -1,26 +1,37 @@
 # Simulate phenology process
 
-
 # Assume fixed probability of transition, bernoulli
-transition <- function(p = 0.1, state1 = 0, state2 = 1) { #simulate the transition between two states and record the states.
-    ptrans <- rbinom(1, size = 1, prob = p) #y/n transition from initial to next stage
-    x <- c("pre")
-    while (ptrans == 0) {
-        x <- append(x, state1)
-        ptrans <- rbinom(1, size = 1, prob = p)
+transition <- function(state1 = 0, state2 = 1, covariate = heatsum) { #simulate the transition between two states and record the states.
+    ptrans <- 0 #initial probability of transition
+    x <- c(state1)
+    y <- c(ptrans)
+    i <- 1
+    while (ptrans < 1) {
+        x <- append(x, state1) #record state1
+        p <- 0.01 * covariate[i] #assume threshold heatsum is 100
+        y <- append(y, p) #record prob
+        ptrans <- rbinom(1, size = 1, prob = p) # sample for new transition probability
+        i <- i + 1
     }
-    x <- append(x, state2)
-    return(x)
+    x <- append(x, state2) # record transition to state2
+
+    return(list(states = x, probabilities = y))
 }
 
-seriesbuilder <- function(p = 0.1, q = 0.1) {
-    firsttrans <- transition(p, state1 = 0, state2 = 1)
-    secondtrans <- transition(q, state1 = 1, state2 = 2)
+seriesbuilder <- function(covariate) {
+    firsttrans <- transition(covariate, state1 = 0, state2 = 1)
+    secondtrans <- transition(covariate, state1 = 1, state2 = 2)
     series <- c(firsttrans, secondtrans)
 }
 
+#simulate temperature data
+temp = rep(5, 50) #constant daily temperature of 5 degrees
+heatsum <- cumsum(temp)
+
 for (i in c(1:10)) {
-    x <- seriesbuilder()
+    x <- seriesbuilder(heatsum)
+    print(c("length", length(x[[1]])))
     print(x)
 }
+
 
