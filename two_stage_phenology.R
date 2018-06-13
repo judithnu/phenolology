35a8599f9f88d9354712efbaf0123575bb5ff99c
Susannah <susannah.tysor@gmail.com>
2018-06-13 12:01:51 -0700

    Working through MacElreath ordered categorical model with my simulated data

--- two_stage_phenology.R
+++ two_stage_phenology.R
@@ -1,22 +1,41 @@
-## This is code from dave
-library(brms, quietly = TRUE)
-set.seed(1)
+library(rethinking)
+phenofakes$state <- phenofakes$state+1
+simplehist(phenofakes$state)
 
-phenofakes$state <- phenofakes$state + 1
-fit = brm(state ~ heatsum,
-    family = cumulative(),
-   # prior = set_prior("student_t(3, 0, 25)", class = "b"),
-    data = phenofakes,
-   chains = 4,
-    cores = 4,
-    #refresh = 50,
-    save_model = "brm.stan"
+
+pr_s <- table(phenofakes$state)/nrow(phenofakes)
+cum_pr_s <- cumsum(pr_s)
+plot(1:3, cum_pr_s, type = "b", xlab="response", ylab="cumulative proportion", ylim=c(0,1))
+
+logit <- function(x) log(x/(1-x))
+lco <- logit(cum_pr_s)
+
+m_intercept_only <- map2stan(
+    alist(
+        state ~ dordlogit(phi, cutpoints),
+        phi <- 0,
+        cutpoints ~ dnorm(0,10)
+          ),
+    data=list(state=phenofakes$state),
+    start=list(cutpoints=c(-1,0)),
+    chains = 2, cores = 2
+
 )
 
-saveRDS(fit, file = "fit.rds")
-fit <- readr::read_rds("fit.rds")
+precis(m_intercept_only, depth=2)
+logistic(coef(m_intercept_only))
 
+m_hs <- map2stan(
+    alist(
+        state ~ dordlogit(phi, cutpoints),
+        phi <- b*heatsum,
+        b ~ dnorm(0,10),
+        cutpoints ~ dnorm(0,10)
+    ),
+    data=phenofakes,
+    start=list(cutpoints=c(-1,0)),
+    chains = 2, cores = 2
+    )
 
-plot(fit, pars = c("heatsum"))
-
-graph_probs <- posterior_linpred(fit, transform = TRUE, nsamples = 2000)
+precis(m_hs, depth = 2)
+logistic(coef(m_hs))
