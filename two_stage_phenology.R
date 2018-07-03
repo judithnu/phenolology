ba9539dcc8e84afa71b41b8c37a197cc3a3eecef
Susannah <susannah.tysor@gmail.com>
2018-07-02 20:48:11 -0700

    simulated data from an ordered logit model

--- two_stage_phenology.R
+++ two_stage_phenology.R
@@ -1,31 +1,48 @@
 library(rethinking)
-phenofakes$state <- phenofakes$state+1
-simplehist(phenofakes$state)
 
+# Simulate phenology data from an ordered logit model for phenology with 3 states, transitions at heatsums of 60 and 120 and a transition speed of 0.1 (in a model logstic model with the exponent parameterized k(x-h), h is at 60 and 120 and k is 0.1 info and equations used for earlier process simulation
+maxtemp = 150
+heatsums <- seq(from=1, to = maxtemp, length.out = 1000)
+h = c(60,120) # temp thresholds
+k = 0.1 # transition speed
+phi <- function(x) k*x #x is heatsum
+a = k*h
 
-pr_s <- table(phenofakes$state)/nrow(phenofakes)
-cum_pr_s <- cumsum(pr_s)
-plot(1:3, cum_pr_s, type = "b", xlab="response", ylab="cumulative proportion", ylim=c(0,1))
+#s1 <- rordlogistic(n=1e4, phi = heatsums , a = a)
+s1 <- rordlogit(1e4, phi = phi(heatsums), a = a)
+hist(s1)
+#p1 <- dordlogistic(s1, phi = heatsums, a = a)
+l1 <- dordlogit(s1, phi = phi(heatsums), a = a) # probability of being in a given state at a given heatsum
+dens(l1)
 
-logit <- function(x) log(x/(1-x))
-lco <- logit(cum_pr_s)
+p1 <- pordlogit(1:3, a = a, phi = phi(heatsums)) #cum probability of _being_ in a given state
+colnames(p1) <- c('p1_s1', 'p1_s2', 'p1_s3')
+p1 <- data.frame(heatsums, p1)
+dens(p1)
 
-m_intercept_only <- map2stan(
-    alist(
-        state ~ dordlogit(phi, cutpoints),
-        phi <- 0,
-        cutpoints ~ dnorm(0,10)
-          ),
-    data=list(state=phenofakes$state),
-    start=list(cutpoints=c(-1,0)),
-    chains = 2, cores = 2
+d <- data.frame(heatsums, s1, l1) %>%
+    dplyr::inner_join(p1, by = 'heatsums')
+d$s1 <- as.factor(d$s1)
 
-)
+dp <- d %>% gather('p1_s1','p1_s2', 'p1_s3', key='state', value='probability')
 
-precis(m_intercept_only, depth=2)
-logistic(coef(m_intercept_only))
+library(viridis)
+ggplot(d, aes(x = heatsums, y = s1, color = s1)) +
+    geom_point() +
+    scale_color_viridis(discrete = TRUE)
 
-m_hs <- map2stan(
+ggplot(d, aes(x = heatsums, y = l1, colour = s1)) +
+    geom_point() +
+    scale_color_viridis(discrete = TRUE)
+
+ggplot(dp, aes(x = heatsums, y = probability, color = state)) +
+    geom_point() +
+    scale_color_viridis(discrete = TRUE)
+
+ggplot(dp, aes(x = probability, y = s1, color = state)) +
+    geom_point() +
+    scale_color_viridis(discrete = TRUE)
+
     alist(
         state ~ dordlogit(phi, cutpoints),
         phi <- b*heatsum,
