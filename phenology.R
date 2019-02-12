778a31bc59b13cf527d81331bb10cd537454b130
Susannah <susannah.tysor@gmail.com>
2019-02-12 12:25:34 -0800

    Wrote a simple generative model for ordered distribution and drafted an ordered logistic model

--- phenology.R
+++ phenology.R
@@ -13,10 +13,16 @@ util <- new.env()
 source('stan_utility.R', local=util)
 
 ############################################################
-# model
+# 1. Conceptual Analysis -------------------------
 ############################################################
+# I want to be able to predict when trees will flower and for how long. Flowering is controlled by heatsum, but may also have a genetic component. The relationship between heatsum and flowering is logistic. In, a logistic model of transition between phenophases with logit(-k(xi-h)) where xi is the heatsum for a given record, h describes the heatsum at which there is a 50% chance an individual tree has begun transitioned between phases (or the point at which 50% of the trees in a population have transitioned between phases). k describes how quickly trees transition from one to another. Trees can only go from 1 to 2 to 3 - they cannot go backwards. An ordered logistic distribution keeps transitions in order.
 
-phenology_data <- read.csv('~/Documents/research_phd/data/PhenologyAndPollenCounts/data_formatted/derived_phenophase.csv', header = TRUE, stringsAsFactors = FALSE)
+# Model is usually written as logit(ak - b*xi) where a is the "intercept" or cutpoint for each transition. to convert it to h, divide b by a.
+
+############################################################
+# 2. Define Observations ----------------------------
+############################################################
+phenology_data <- read.csv('~/Documents/research_phd/data/PhenologyAndPollenCounts/data_formatted_and_derived//derived_phenophase.csv', header = TRUE, stringsAsFactors = FALSE)
 
 pd <- phenology_data
 
@@ -43,3 +49,37 @@ lco <- logit(cum_pr_pd) #log cumulative odds
 # logistic <- function(x) exp(x)/(1 + exp(x))
 # logistic(cum_pr_pd)
 # logistic(pr_pd)
+
+############################################################
+# 3. Identify Relevant Summary Statistics
+############################################################
+# h and k are relevant summary statistics.
+
+
+############################################################
+# 4. Build a Generative Model
+############################################################
+writeLines(readLines("~/Documents/research_phenolology/phenology.stan"))
+gen_model <- "~/Documents/research_phenolology/phenology.stan"
+############################################################
+# 5. Analyze the Generative Ensemble
+############################################################
+R <- 100 # 1000 draws from the Bayesian joint distribution
+N <- 3
+#theta <- c(1,1,1)/sum(c(1,1,1))
+
+############################################################
+# 5a. Analyze the Prior Predictive Distribution
+############################################################
+simu_data <- list("N" = N, "theta" = theta)
+
+fit <- stan(file=gen_model, data=simu_data,
+            iter=R, warmup=0, chains=1, refresh=R,
+            seed=4838282, algorithm="Fixed_param")
+
+simu_ys <- extract(fit)$y
+hist(simu_ys)
+
+############
+
+N <- nrow(pd)
--- phenology.stan
+++ phenology.stan
@@ -1,19 +1,44 @@
+// data{
+// int<lower=2> after;
+// int<lower=1> flowering;
+// int<lower=0> before;
+// int<lower=1, upper=after> y[N]
+// row_vector[flowering] x[N]
+// }
+//
+// parameters {
+//     vector[flowering] beta;
+//     ordered[after - 1] c; //cutpoints
+// }
+
 data{
-int<lower=2> after;
-int<lower=1> flowering;
-int<lower=0> before;
-int<lower=1, upper=after> y[N]
-row_vector[flowering] x[N]
+    int<lower=2> K; // number of possible phenophases
+    int<lower=0> N; // number of phenophases observations
+    int<lower=1, upper=K> y[N]; //phenophase outcomes (response)
+
+    int<lower=0> D; //number of heatsum observations
+    row_vector[D] x[N]; //heatsums
 }
 
-parameters {
-    vector[flowering] beta;
-    ordered[after - 1] c; //cutpoints
+parameters{
+    vector[D] beta;
+    ordered[K-1] c; //cutpoints mean
 }
 
-generated quantities {
-    ordered_logistic_rng(x[n] * beta, c)
+model {
+    for (n in 1:N)
+      y[n] ~ ordered_logistic(x[n] * beta, c);
 }
+
+
+// generated quantities {
+//     // Simulate model configuration from prior model
+//
+//     //Simulate data from observational model
+//     int y[N] = rep_array(0,N);
+//     for (n in 1:N)
+//       y[n] = ordered_logistic_rng(theta);
+// }
 
 // model {
 //     for (n in 1:N)
