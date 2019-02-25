767d8db8679aa9ff503acbec0a5842693820a6ed
Susannah <susannah.tysor@gmail.com>
2019-02-24 16:59:51 -0800

    Ran simplest version of ordered logistic model for male and female at Prince George

--- phenology.R
+++ phenology.R
@@ -21,7 +21,7 @@ source('~/Documents/classes/STANworkshop/material/1 - workflow/stan_utility.R', 
 # Model is usually written as logit(ak - b*xi) where a is the "intercept" or cutpoint for each transition. to convert it to h, divide b by a.
 
 ############################################################
-# 2. Define Observations ----------------------------
+# 2. Define Observations
 ############################################################
 phenology_and_heatsum_data <- read.csv('~/Documents/research_phenolology/data/phenology_heatsum.csv', stringsAsFactors = FALSE)
 
@@ -115,6 +115,8 @@ sim_fit <- stan(file='phenology.stan', data=sim_data_for_stan,
 
 util$check_all_diagnostics(sim_fit)
 
+# VISUAL MCMC DIAGNOSTICS
+############################################################
 params <- extract(sim_fit)
 posterior_cp <- as.array(sim_fit) #extract posterior draws
 lp_cp <- log_posterior(sim_fit)
@@ -168,6 +170,90 @@ mcmc_neff(ratios_cp, size = 2) +
 #n_eff/N decreases as autocorrelation becomes more extreme. Visualize autocorrelation using mcmc_acf or mcmc_acf_bar. Postive autocorrelation is bad because it means the chain gets stuck. Ideally, it drops quickly to zero as lag increasses. negative autocorrelation indicates fast convergence of sample mean towards true
 
 mcmc_acf(posterior_cp, lags = 10)
-############
 
-N <- nrow(pd)
+
+# PARAMETER ESTIMATES
+############################################################
+# Plotting MCMC draws
+
+posterior <- as.array(sim_fit)
+dim(sim_fit)
+
+dimnames(posterior)
+
+### Posterior uncertainty intervals ("credible intervals")
+
+# central posterior uncertainty intervals. by default shows 50% intervals (thick) and 90% intervals (thinner outer). Use point_est to show or hide point estimates
+color_scheme_set("red")
+mcmc_intervals(posterior, pars = c("c[1]", "c[2]", "beta"))
+
+# show uncertainty intervals as shaded areas under estimated posterior density curves
+
+mcmc_areas(
+    posterior,
+    pars = c("c[1]", "c[2]", "beta"),
+    prob = 0.8, # 80% intervals
+    prob_outer = 0.99, # 99% intervals
+    point_est = "mean"
+)
+
+### Univariate marginal posterior distributions
+# Look at histograms or kernel density estimates of marginal posterior distributions, chains together or separate
+
+# plot marginal posterior distributions combining all chains. Tranformations argument can be helpful
+color_scheme_set("green")
+mcmc_hist(posterior, pars = c("c[1]", "c[2]", "beta"))
+
+color_scheme_set("brightblue")
+mcmc_hist_by_chain(posterior, pars = c("c[1]", "c[2]", "beta"))
+
+# plot densities rather than histograms
+color_scheme_set("purple")
+mcmc_dens(posterior, pars = c("c[1]", "c[2]", "beta"))
+    # by chain
+mcmc_dens_overlay(posterior, pars = c("c[1]", "c[2]", "beta"))
+
+# plot violins
+
+color_scheme_set("teal")
+mcmc_violin(posterior, pars = c("c[1]", "c[2]", "beta"), probs = c(0.1, 0.5, 0.9))
+
+### Bivariate plots
+
+# just a scatterplot of two params
+color_scheme_set("gray")
+mcmc_scatter(posterior, pars = c("c[1]", "c[2]"))
+mcmc_scatter(posterior, pars = c("c[1]", "beta"))
+mcmc_scatter(posterior, pars = c("c[2]", "beta"))
+
+# hexagonal binner helps avoid overplotting
+mcmc_hex(posterior, pars = c("c[1]", "c[2]"))
+mcmc_hex(posterior, pars = c("c[1]", "beta"))
+mcmc_hex(posterior, pars = c("c[2]", "beta"))
+
+# with more than two params. use hex plots instead with off_diag_fun = "hex". half markov chains above and half below diagonal
+color_scheme_set("pink")
+mcmc_pairs(posterior, pars = c("c[1]", "c[2]", "beta"), off_diag_args = list(size = 1.5))
+
+### Trace plots
+# no divergence info (see mcmc diagnostics graphs)
+
+color_scheme_set("blue")
+mcmc_trace(posterior, pars = c("c[1]", "c[2]", "beta"))
+
+# with mixed colour scheme
+
+color_scheme_set("mix-blue-red")
+mcmc_trace(posterior, pars = c("c[1]", "c[2]", "beta"),
+           facet_args = list(ncol=1, strip.position = "left")) #facet_arg passes parameters to facet_wrap in ggplot2. ncol puts traceplots in a column instead of side by side and strip.position moves the facet labels
+
+# with viridis colour scheme
+color_scheme_set("viridis")
+mcmc_trace(posterior, pars = c("c[1]", "c[2]", "beta"))
+
+# use points instead of lines and highlight a single chain
+mcmc_trace_highlight(posterior, pars = c("c[1]", "c[2]", "beta"), highlight = 3)
+
+
+
+# ------------------
