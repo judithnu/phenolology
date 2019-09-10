12043eeb6ddac04bc76d10ecd72df6a0ec32cd25
Susannah <susannah.tysor@gmail.com>
2019-09-10 16:22:44 -0700

    removed unneeded graphs and repeated good graph code from provs for sites

--- FUs_to_DoY.R
+++ FUs_to_DoY.R
@@ -87,7 +87,7 @@ ggplot(filter(tpars_wide, effect=="all"), aes(x=Sex, y=length, fill=WeatherRegim
 #     scale_fill_viridis_d() +
 #     facet_grid(rows=vars(Sex,param), cols=vars(WeatherRegime), scales="free_y")
 
-# compare with and without provenance effect
+# compare with and without provenance effect #############
 
 phd1 <- filter(tparsgraph, effect == "all") %>%
     select(-sum_forcing, -effect)
@@ -97,24 +97,16 @@ phd2 <- filter(tparsgraph, effect == "no_prov") %>%
 provcompd <- full_join(phd1, phd2) %>%
     mutate(daydiff = DoY_noprov-DoY)
 
-ggplot(provcompd, aes(x=DoY, fill="prov", group=side, linetype=WeatherRegime)) +
-    geom_density() +
-    geom_density(aes(x=DoY_noprov, fill="no_prov", group=side, linetype=WeatherRegime), alpha=0.5) +
-    scale_fill_viridis_d(option="B", begin = .1, end=.9) +
-    facet_grid(SPU_Name ~ Sex) +
-    ggtitle("Provenance effect DoY") +
-    xlab("Day of Year") +
-    theme_bw(base_size=18) +
-    theme(strip.text.y = element_text(angle = 0)) +
-    theme(legend.position= "top")
-
-ggplot(provhalfd, aes(x=param, y=daydiff, fill=WeatherRegime, color=WeatherRegime)) +
-    geom_boxplot() +
-    facet_grid(SPU_Name ~ Sex, scales="free_y") +
-    scale_fill_viridis_d(option="B", begin=0.3, end=0.8) +
-    scale_color_viridis_d(option="B", begin=0.3, end=0.8) +
-    ggtitle("") +
-    theme_bw()
+# ggplot(provcompd, aes(x=DoY, fill="prov", group=side, linetype=WeatherRegime)) +
+#     geom_density() +
+#     geom_density(aes(x=DoY_noprov, fill="no_prov", group=side, linetype=WeatherRegime), alpha=0.5) +
+#     scale_fill_viridis_d(option="B", begin = .1, end=.9) +
+#     facet_grid(SPU_Name ~ Sex) +
+#     ggtitle("Provenance effect DoY") +
+#     xlab("Day of Year") +
+#     theme_bw(base_size=18) +
+#     theme(strip.text.y = element_text(angle = 0)) +
+#     theme(legend.position= "top")
 
 
 hpd_lower = function(x, prob) rethinking::HPDI(x, prob)[1]
@@ -168,7 +160,8 @@ ggplot(filter(trans2, interval=="0.99"), aes(x=value, y=y, color=WeatherRegime, 
     theme(strip.text.y = element_text(angle = 0), axis.text.y = element_blank(), axis.ticks = element_blank(), legend.position="top") +
     xlab("Day Differences") +
     ylab("") +
-    guides(size=FALSE, colour=guide_legend(override.aes = list(size=3)))
+    guides(size=FALSE, colour=guide_legend(override.aes = list(size=3)))+
+    xlim(c(-20,20))
 
 # calculate HPDIs for DoY diffs SITE #############
 
@@ -191,6 +184,42 @@ fifty <- group_by(sitecompd, side, Sex, Site, WeatherRegime) %>%
 hpd_df <- full_join(full, fifty) %>%
     gather(key=end, value=value, x, xend) %>%
     arrange(interval)
+
+
+hpd_df <- mutate(hpd_df, y = case_when(WeatherRegime=="DoY_KAL1998" ~ 0,
+                                       WeatherRegime=="DoY_KR2011" ~ 1))
+
+trans1 <- filter(hpd_df, side=="begin")
+ggplot(filter(trans1, interval=="0.99"), aes(x=value, y=y, color=WeatherRegime, size=1)) +
+    geom_line() +
+    geom_line(data = filter(trans1, interval=="0.75"), aes(x=value, y=y, size=1.5)) +
+    facet_grid(Site ~ Sex) +
+    theme_bw(base_size=16) +
+    scale_color_viridis_d(option="C", end=0, begin=0.7, labels=c("hot year", "cold year")) +
+    ylim(c(-1, 2)) +
+    geom_vline(xintercept=0, alpha=0.7) +
+    ggtitle("Site Effects in Days in Cold and Hot Years", subtitle = "Start Day. 75 and 95% HPDI. Negative is days earlier, positive is days later.") +
+    theme(strip.text.y = element_text(angle = 0), axis.text.y = element_blank(), axis.ticks = element_blank(), legend.position="top") +
+    xlab("Day Differences") +
+    ylab("") +
+    guides(size=FALSE, colour=guide_legend(override.aes = list(size=3)))+
+    xlim(c(-20,20))
+
+trans2 <- filter(hpd_df, side=="end")
+ggplot(filter(trans2, interval=="0.99"), aes(x=value, y=y, color=WeatherRegime, size=1)) +
+    geom_line() +
+    geom_line(data = filter(trans1, interval=="0.75"), aes(x=value, y=y, size=1.5)) +
+    facet_grid(SPU_Name ~ Sex) +
+    theme_bw(base_size=16) +
+    scale_color_viridis_d(option="C", end=0, begin=0.7, labels=c("hot year", "cold year")) +
+    ylim(c(-1, 2)) +
+    geom_vline(xintercept=0, alpha=0.7) +
+    ggtitle("Provenance Effects in Days in Cold and Hot Years", subtitle = "End Day. 75 and 95% HPDI. Negative is days earlier, positive is days later.") +
+    theme(strip.text.y = element_text(angle = 0), axis.text.y = element_blank(), axis.ticks = element_blank(), legend.position="top") +
+    xlab("Day Differences") +
+    ylab("") +
+    guides(size=FALSE, colour=guide_legend(override.aes = list(size=3)))+
+    xlim(c(-20,20))
 
 # Graph DoY start and finish ##################
 
