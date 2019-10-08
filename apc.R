6d1887f0810b899d5b78b3c949a6ce6c03231d68
Susannah <susannah.tysor@gmail.com>
2019-10-08 08:29:39 -0700

    started average predictive comparisons

---
+++ apc.R
@@ -0,0 +1,25 @@
+# Average Predictive comparisons
+
+fmod <- readRDS("slopes_nc_scaled_ristos_FEMALE2019-10-04climatena.rds") %>%
+    as.data.frame()
+
+#mmod <- readRDS("slopes_nc_scaled_ristos_MALE2019-10-04climatena.rds") %>%
+    as.data.frame()
+
+state_repf <- t(as.matrix(dplyr::select(fmod, contains("state_rep"))))
+#state_repm <- t(as.matrix(dplyr::select(mmod, contains("state_rep"))))
+
+reps <- c(1:1800)
+#colnames(state_repm) <- paste("staterep", reps, sep="_")
+colnames(state_repf) <- paste("staterep", reps, sep="_")
+
+# original data (this code should match relevant bits in run_stan)
+phendf <- read_data()
+
+# identify combinations of effects that actually occur
+
+fdf <- splitsex(phendf, "FEMALE")
+#mdf <- splitsex(phendf, "MALE")
+
+
+#udf <- unique_grouper(fdf, mdf)
