a97e547990056dd54a46095eeb7b5621d7935247
Susannah <susannah.tysor@gmail.com>
2019-10-31 10:24:11 -0700

    moved functions from script to library

--- phenology_functions.R
+++ phenology_functions.R
@@ -33,7 +33,7 @@ read_data <- function(slim=TRUE) { #choose true if slimmed data and false for fu
                                    stringsAsFactors = FALSE, header = TRUE
         )
     }
-    
+
 
     ## provenance
     SPU_dat <- read.csv("../phd/data/OrchardInfo/LodgepoleSPUs.csv",
@@ -71,4 +71,58 @@ unique_grouper <- function(df1 = fdf, df2 = mdf) {
     udf$IndSexGroup <- group_indices(udf, Sex, IndGroup)
     return(udf)
 }
+
+# Build a dataframe where each row is a unique combination of parameters for each unique combination of sex, site, clone, year, and provenance that occurs in the data (IndSexGroup). #pardf is a dataframe with N rows of parameters for each sum_forcing, site, provenance, clone, and year combination that appear in the data, where N = number of draws from the posterior distribution
+build_par_df <- function(mcmcdf, datdf = udf, sex) {
+
+    udf <- dplyr::filter(datdf, Sex==sex)
+    mcmcdf$iter <- 1:nrow(mcmcdf)
+
+    singledimpars <- mcmcdf %>%
+        dplyr::select(iter, beta, sigma_site, sigma_prov, sigma_clone, sigma_year, contains("kappa"), contains("mean")) %>%
+        rename(kappa1 = `kappa[1]`) %>%
+        rename(kappa2 = `kappa[2]`)
+
+    siteb <- tidypar(mcmcdf, "b_site", "SiteID")
+    provb <- tidypar(mcmcdf, "b_prov", "ProvenanceID")
+    cloneb <- tidypar(mcmcdf, "b_clone", "CloneID")
+    yearb <- tidypar(mcmcdf, "b_year", "YearID")
+
+    clonemerge <- left_join(udf, cloneb) %>%
+        select(-name)
+    pardf <- left_join(udf, clonemerge)
+    rm(clonemerge, cloneb)
+    sitemerge <- left_join(udf, siteb) %>%
+        select(-name)
+    pardf <- left_join(pardf, sitemerge)
+    rm(sitemerge, siteb)
+    provmerge <- left_join(udf, provb) %>%
+        select(-name)
+    pardf <- left_join(pardf, provmerge)
+    rm(provmerge, provb)
+    yearmerge <- left_join(udf, yearb) %>%
+        select(-name)
+    pardf <- left_join(pardf, yearmerge)
+    rm(yearmerge, yearb)
+    pardf <- left_join(pardf, singledimpars)
+
+    # pardf <- left_join(udf, clonemerge) %>%
+    #     left_join(provmerge) %>%
+    #     left_join(sitemerge) %>%
+    #     left_join(yearmerge) %>%
+    #     left_join(singledimpars)
+}
+
+#take a stan model dataframe and create a tidy dataframe for a given parameter. takes a dataframe, a string with the parameter name, and a string describing the param ID (e.g. par = "Site" and id="SiteID")
+tidypar <- function(stanframe, param, id) {
+    #wide to long format for parameters
+    par <- stanframe %>%
+        dplyr::select(contains(param), iter) %>%
+        tidyr::gather(key = key, value = value, contains("b_")) %>%
+        dplyr::mutate(id = str_extract(key, "[0-9]{1,}"))
+    colnames(par) <- c("iter", "name", param, id)
+    par[,ncol(par)] <- as.integer(par[,ncol(par)])
+    return(par)
+}
+
 
