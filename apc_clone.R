ff045d9eb62a55788963a28ce95024652033e53a
Susannah <susannah@pobox.com>
2019-11-10 20:28:48 -0800

    Separate out apc calculation by variable
    so can run in parallel as background jobs.

---
+++ apc_clone.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+library(gtools)
+library(assertthat)
+
+clone_apc <- calc_apc_of_variable(u="b_clone", uid = "CloneID", v=c("b_site", "b_prov", "b_year"), 
+                                  vid = c("SiteID", "ProvenanceID", "YearID"), 
+                                  phendata = udf, model_params = pardf)
---
+++ apc_year.R
@@ -0,0 +1,7 @@
+library(tidyverse)
+library(gtools)
+library(assertthat)
+
+year_apc <- calc_apc_of_variable(u="b_year", uid = "YearID", v=c("b_site", "b_prov", "b_clone"), 
+                                 vid = c("SiteID", "ProvenanceID", "CloneID"), 
+                                 phendata = udf, model_params = pardf)
