47adcaa2f4aa885e4fedf033189df72d977b6e47
Susannah <susannah.tysor@gmail.com>
2019-11-13 21:19:03 -0800

    predicted start and end from model and graphed

--- predictfrommodel.R
+++ predictfrommodel.R
@@ -4,63 +4,115 @@
 # Need parameter values (distributions?)
 # Need accumulated forcing time series - use existing for now.
 
-# No generated quantities block. I'm calculating fstart and fend, right? so I can just use the values for b_*_mean and sigma_* that I pulled 
+# No generated quantities block. I'm calculating fstart and fend, right? so I can just use the values for b_*_mean and sigma_* that I pulled
 # from the posterior when I simulated the model in the first place.
+
+library(tidyverse)
+library(gtools)
+
+source('phenology_functions.R')
 
 
 fmod_raw <- readRDS("2019-10-28phenologyFEMALE.rds") %>%
   as.data.frame() %>%
-  select(-contains("state_rep")) 
+  select(-contains("state_rep"))
 
 mmod_raw <- readRDS("2019-10-28phenologyMALE.rds") %>%
   as.data.frame() %>%
   select(-contains("state_rep"))
 
+# GLOBALS ###########
+forcingtype="scaled_ristos"
+
+# MODELS #############
 fmod <- fmod_raw %>%
   select(contains("kappa"), beta, contains("mean"))
 fmod$iter <- 1:nrow(fmod)
+rm(fmod_raw)
 mmod <- mmod_raw %>%
   select(contains("kappa"), beta, contains("mean"))
 mmod$iter <- 1:nrow(mmod)
+rm(mmod_raw)
 
-
-calc_fstart_means <- 
-calc_fstart <- function(df) {
-  forcing <- with(df, {
-    (logit(0.2) + kappa1)/(beta + b_site + b_prov + b_clone + b_year)
-  })
-  return(forcing)
-}
-
-fpredict <- fmod %>% 
-  mutate(start = (logit(0.2) + `kappa[1]`)/(beta + b_site_mean + b_clone_mean + b_year_mean + b_prov_mean)) %>%
+# FORCING PREDICTIONS #############
+fpredict <- fmod %>%
+  mutate(begin = (logit(0.2) + `kappa[1]`)/(beta + b_site_mean + b_clone_mean + b_year_mean + b_prov_mean)) %>%
   mutate(end = (logit(0.8) + `kappa[2]`)/(beta + b_site_mean + b_clone_mean + b_year_mean + b_prov_mean)) %>%
   mutate(Sex="FEMALE") %>%
-  select(iter, start, end, Sex)
+  select(iter, begin, end, Sex)
 
 mpredict <- mmod %>%
-  mutate(start = (logit(0.2) + `kappa[1]`)/(beta + b_site_mean + b_clone_mean + b_year_mean + b_prov_mean)) %>%
+  mutate(begin = (logit(0.2) + `kappa[1]`)/(beta + b_site_mean + b_clone_mean + b_year_mean + b_prov_mean)) %>%
   mutate(end = (logit(0.8) + `kappa[2]`)/(beta + b_site_mean + b_clone_mean + b_year_mean + b_prov_mean)) %>%
   mutate(Sex="MALE") %>%
-  select(iter, start, end, Sex)
+  select(iter, begin, end, Sex)
 
 predictphen <- rbind(fpredict, mpredict) %>%
-  pivot_longer(cols=c("start", "end"), names_to = "side", values_to = "accumulated_forcing" )
+  pivot_longer(cols=c("end", "begin"), names_to = "side", values_to = "sum_forcing" )
 
-ggplot(predictphen, aes(x=accumulated_forcing, fill=Sex, linetype=side)) +
+ggplot(predictphen, aes(x=sum_forcing, fill=Sex, linetype=side)) +
   geom_density(alpha=0.5) +
   scale_fill_viridis_d(end=0.8) +
   ggtitle("Forcing required to begin and end flowering") +
   theme_bw(base_size=18)
 
-## Climate data ####
+# Climate data ####
 clim <- read.csv("data/all_clim_PCIC.csv", header=TRUE, stringsAsFactors=FALSE) %>%
   filter(forcing_type==forcingtype)
 clim$siteyear <- paste(clim$Site, clim$Year, sep='')
 
-mpredict
+# CLIMATE PREDICTIONS ###########
+splitclim <- split(clim, clim$siteyear)
 
-clim
- climset$DoY[findInterval(mpredict$start,
-                                 climset$sum_forcing) + 1]
- 
+
+
+ifinder <-  function(x) {
+   index <- findInterval(predictphen$sum_forcing, x$sum_forcing)
+   doy <- x$DoY[index]
+   df <- data.frame(iter=predictphen$iter, DoY=doy, siteyear=x$siteyear[1], Sex=predictphen$Sex, side=predictphen$side, sum_forcing=predictphen$sum_forcing)
+   return(df)
+ }
+
+DoYpredict <- purrr::map_df(splitclim, ifinder)
+assertthat::assert_that(nrow(DoYpredict)==nrow(predictphen) * length(splitclim))
+
+ryears <- sample(unique(clim$siteyear), 30)
+ggplot(filter(DoYpredict, siteyear %in% ryears), aes(x=siteyear, y=DoY, color=Sex)) +
+  geom_violin() +
+  facet_grid(. ~ side)+
+  theme(legend.position = "none") +
+  scale_color_viridis_d(end=0.8) +
+  scale_x_discrete(labels=NULL)
+
+ggplot(filter(DoYpredict, siteyear %in% ryears), aes(x=DoY, fill=Sex)) +
+  geom_density(alpha=0.5) +
+  facet_grid(. ~ side)+
+  theme(legend.position = "none") +
+  scale_fill_viridis_d(end=0.8) +
+  scale_x_discrete(labels=NULL)
+
+phendf <- read_data(slim = FALSE)
+
+firstandlastrf <- phendf %>%
+  group_by(Index, Sex) %>%
+  filter(Phenophase_Derived==2) %>%
+  summarise(firstrf=min(DoY), lastrf=max(DoY)) %>%
+  pivot_longer(cols=contains("rf"), names_to = "beginorend", values_to = "DoY") %>%
+  left_join(select(phendf, Index, Sex, DoY, sum_forcing))
+
+ggplot(sample_frac(DoYpredict,0.2), aes(x=sum_forcing, y=DoY)) +
+  #stat_binhex(binwidth=c(0.2, 1), alpha=0.7) +
+  geom_density_2d(color="gray") +
+  #geom_jitter(data=filter(phendf, Phenophase_Derived==2), aes(x=sum_forcing, y=DoY, color=Sex), alpha=0.1, pch=1) +
+  #geom_jitter(data=firstandlastrf, aes(x=sum_forcing, y=DoY, color=beginorend), alpha=0.2, pch=1) +
+  geom_density_2d(data=firstandlastrf, aes(x=sum_forcing, y=DoY, color=beginorend))+
+  facet_grid(Sex ~ .) +
+  scale_color_viridis_d(option="A", end=0.8) +
+  theme_bw(base_size=18) +
+  theme(legend.position = "none") +
+  ylab("Day of Year") +
+  xlab("accumulated forcing") +
+  ggtitle("Flowering start and end date", subtitle="forcing accumulation and day of year")
+
+
+# plot stage 1 and 3 in above graph, too?
